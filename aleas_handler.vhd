fdd9643777e5c91d731e152be5eeb0805e28e912
Benoît Morgan <bntmorgan@gmail.com>
2012-05-27 16:59:20 +0200

    * Aléas gérés pour les opérations airthmétiques les COP et les AFC * Marche peut-être pour le LOAD et le STORE (ajoutés dans le process de contrôle)

--- aleas_handler.vhd
+++ aleas_handler.vhd
@@ -19,6 +19,8 @@
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -46,29 +48,48 @@ end aleas_handler;
 
 architecture Behavioral of aleas_handler is
 
+signal wait_one : STD_LOGIC := '0';
+
 begin
 
+
+
 clk_out <= '0' when (((((ex_mem_a = li_di_b) or (ex_mem_a = li_di_c)) and (li_di_op = x"01" or li_di_op = x"02" or li_di_op = x"03" or li_di_op = x"04")) or 
-                     (ex_mem_a = li_di_b and (li_di_op = x"05" or li_di_op = x"08"))) and 
-                     (ex_mem_op = x"01" or ex_mem_op = x"02" or ex_mem_op = x"03" or ex_mem_op = x"04" or ex_mem_op = x"05" or ex_mem_op = x"06" or ex_mem_op = x"07")) or 
+                      (ex_mem_a = li_di_b and (li_di_op = x"05" or li_di_op = x"08"))) and 
+                      (ex_mem_op = x"01" or ex_mem_op = x"02" or ex_mem_op = x"03" or ex_mem_op = x"04" or ex_mem_op = x"05" or ex_mem_op = x"06" or ex_mem_op = x"07")) or 
                     (((((mem_re_a = li_di_b) or (mem_re_a = li_di_c)) and (li_di_op = x"01" or li_di_op = x"02" or li_di_op = x"03" or li_di_op = x"04")) or 
-                     (mem_re_a = li_di_b and (li_di_op = x"05" or li_di_op = x"08"))) and 
-                     (mem_re_op = x"01" or mem_re_op = x"02" or mem_re_op = x"03" or mem_re_op = x"04" or mem_re_op = x"05" or mem_re_op = x"06" or mem_re_op = x"07")) or
+                      (mem_re_a = li_di_b and (li_di_op = x"05" or li_di_op = x"08"))) and 
+                      (mem_re_op = x"01" or mem_re_op = x"02" or mem_re_op = x"03" or mem_re_op = x"04" or mem_re_op = x"05" or mem_re_op = x"06" or mem_re_op = x"07")) or
                     (((((di_ex_a = li_di_b) or (di_ex_a = li_di_c)) and (li_di_op = x"01" or li_di_op = x"02" or li_di_op = x"03" or li_di_op = x"04")) or 
-                     (di_ex_a = li_di_b and (li_di_op = x"05" or li_di_op = x"08"))) and 
-                     (di_ex_op = x"01" or di_ex_op = x"02" or di_ex_op = x"03" or di_ex_op = x"04" or di_ex_op = x"05" or di_ex_op = x"06" or di_ex_op = x"07"))
+                      (di_ex_a = li_di_b and (li_di_op = x"05" or li_di_op = x"08"))) and 
+                      (di_ex_op = x"01" or di_ex_op = x"02" or di_ex_op = x"03" or di_ex_op = x"04" or di_ex_op = x"05" or di_ex_op = x"06" or di_ex_op = x"07")) or 
+                    wait_one /= '0'
                else clk;
 
+-- On doit attendre un coup d'horloge une fois l'alea terminé pour recopier l'instruction courante du pipeline
+wait_one <= '1' when ((((((ex_mem_a = li_di_b) or (ex_mem_a = li_di_c)) and (li_di_op = x"01" or li_di_op = x"02" or li_di_op = x"03" or li_di_op = x"04")) or 
+                        (ex_mem_a = li_di_b and (li_di_op = x"05" or li_di_op = x"08"))) and 
+                        (ex_mem_op = x"01" or ex_mem_op = x"02" or ex_mem_op = x"03" or ex_mem_op = x"04" or ex_mem_op = x"05" or ex_mem_op = x"06" or ex_mem_op = x"07")) or 
+                      (((((mem_re_a = li_di_b) or (mem_re_a = li_di_c)) and (li_di_op = x"01" or li_di_op = x"02" or li_di_op = x"03" or li_di_op = x"04")) or 
+                        (mem_re_a = li_di_b and (li_di_op = x"05" or li_di_op = x"08"))) and 
+                        (mem_re_op = x"01" or mem_re_op = x"02" or mem_re_op = x"03" or mem_re_op = x"04" or mem_re_op = x"05" or mem_re_op = x"06" or mem_re_op = x"07")) or
+                      (((((di_ex_a = li_di_b) or (di_ex_a = li_di_c)) and (li_di_op = x"01" or li_di_op = x"02" or li_di_op = x"03" or li_di_op = x"04")) or 
+                        (di_ex_a = li_di_b and (li_di_op = x"05" or li_di_op = x"08"))) and 
+                        (di_ex_op = x"01" or di_ex_op = x"02" or di_ex_op = x"03" or di_ex_op = x"04" or di_ex_op = x"05" or di_ex_op = x"06" or di_ex_op = x"07"))) and rising_edge(clk)
+                 else '0' when wait_one = '1' and rising_edge(clk);
+
 li_di_op_out <= x"00" when (((((ex_mem_a = li_di_b) or (ex_mem_a = li_di_c)) and (li_di_op = x"01" or li_di_op = x"02" or li_di_op = x"03" or li_di_op = x"04")) or 
-                     (ex_mem_a = li_di_b and (li_di_op = x"05" or li_di_op = x"08"))) and 
-                     (ex_mem_op = x"01" or ex_mem_op = x"02" or ex_mem_op = x"03" or ex_mem_op = x"04" or ex_mem_op = x"05" or ex_mem_op = x"06" or ex_mem_op = x"07")) or 
-                    (((((mem_re_a = li_di_b) or (mem_re_a = li_di_c)) and (li_di_op = x"01" or li_di_op = x"02" or li_di_op = x"03" or li_di_op = x"04")) or 
-                     (mem_re_a = li_di_b and (li_di_op = x"05" or li_di_op = x"08"))) and 
-                     (mem_re_op = x"01" or mem_re_op = x"02" or mem_re_op = x"03" or mem_re_op = x"04" or mem_re_op = x"05" or mem_re_op = x"06" or mem_re_op = x"07")) or
-                    (((((di_ex_a = li_di_b) or (di_ex_a = li_di_c)) and (li_di_op = x"01" or li_di_op = x"02" or li_di_op = x"03" or li_di_op = x"04")) or 
-                     (di_ex_a = li_di_b and (li_di_op = x"05" or li_di_op = x"08"))) and 
-                     (di_ex_op = x"01" or di_ex_op = x"02" or di_ex_op = x"03" or di_ex_op = x"04" or di_ex_op = x"05" or di_ex_op = x"06" or di_ex_op = x"07"))
-               else li_di_op;
+                             (ex_mem_a = li_di_b and (li_di_op = x"05" or li_di_op = x"08"))) and 
+                             (ex_mem_op = x"01" or ex_mem_op = x"02" or ex_mem_op = x"03" or ex_mem_op = x"04" or ex_mem_op = x"05" or ex_mem_op = x"06" or ex_mem_op = x"07")) or 
+                           (((((mem_re_a = li_di_b) or (mem_re_a = li_di_c)) and (li_di_op = x"01" or li_di_op = x"02" or li_di_op = x"03" or li_di_op = x"04")) or 
+                             (mem_re_a = li_di_b and (li_di_op = x"05" or li_di_op = x"08"))) and 
+                             (mem_re_op = x"01" or mem_re_op = x"02" or mem_re_op = x"03" or mem_re_op = x"04" or mem_re_op = x"05" or mem_re_op = x"06" or mem_re_op = x"07")) or
+                           (((((di_ex_a = li_di_b) or (di_ex_a = li_di_c)) and (li_di_op = x"01" or li_di_op = x"02" or li_di_op = x"03" or li_di_op = x"04")) or 
+                             (di_ex_a = li_di_b and (li_di_op = x"05" or li_di_op = x"08"))) and 
+                             (di_ex_op = x"01" or di_ex_op = x"02" or di_ex_op = x"03" or di_ex_op = x"04" or di_ex_op = x"05" or di_ex_op = x"06" or di_ex_op = x"07"))
+                      else li_di_op;
 
 end Behavioral;
+
+
 
